79dd6530f9a22b56e47744346466bdabae5df7d5
Unknown <ost.bregin2014@yandex.ua>
2017-11-03 20:22:35 +0200

    Laravel-debugbar is set to work

--- config/app.php
+++ config/app.php
@@ -163,7 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Barryvdh\Debugbar\ServiceProvider::class,
+        
         /*
          * Package Service Providers...
          */
@@ -225,7 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        
     ],
 
 ];
